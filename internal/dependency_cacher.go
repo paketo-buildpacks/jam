2f940fccca1eff0e4c12cdcd64dae635eb6a9c28
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2022-08-06 18:28:58 +0000

    WIP: parallel caching of dependencies.

--- internal/dependency_cacher.go
+++ internal/dependency_cacher.go
@@ -36,41 +36,72 @@ func (dc DependencyCacher) Cache(root string, deps []cargo.ConfigMetadataDepende
 		return nil, fmt.Errorf("failed to create dependencies directory: %s", err)
 	}
 
-	var dependencies []cargo.ConfigMetadataDependency
-	for _, dep := range deps {
+	sem := make(chan error, len(deps)) // semaphore pattern
+
+	// var dependencies []cargo.ConfigMetadataDependency
+	dependencies := make([]cargo.ConfigMetadataDependency, len(deps))
+	for i, dep := range deps {
 		dc.logger.Subprocess("%s (%s) [%s]", dep.ID, dep.Version, strings.Join(dep.Stacks, ", "))
 		dc.logger.Action("↳  dependencies/%s", dep.SHA256)
 
-		source, err := dc.downloader.Drop("", dep.URI)
-		if err != nil {
-			return nil, fmt.Errorf("failed to download dependency: %s", err)
-		}
+		go func(i int, dep cargo.ConfigMetadataDependency) {
+			// dc.logger.Subprocess("%s (%s) [%s]\n↳  dependencies/%s", dep.ID, dep.Version, strings.Join(dep.Stacks, ", "),dep.SHA256)
 
-		validatedSource := cargo.NewValidatedReader(source, dep.SHA256)
+			source, err := dc.downloader.Drop("", dep.URI)
+			if err != nil {
+				sem <- fmt.Errorf("failed to download dependency: %s", err)
+				return
+				// return nil, fmt.Errorf("failed to download dependency: %s", err)
+			}
 
-		destination, err := os.Create(filepath.Join(dir, dep.SHA256))
-		if err != nil {
-			return nil, fmt.Errorf("failed to create destination file: %s", err)
-		}
+			validatedSource := cargo.NewValidatedReader(source, dep.SHA256)
 
-		_, err = io.Copy(destination, validatedSource)
-		if err != nil {
-			return nil, fmt.Errorf("failed to copy dependency: %s", err)
-		}
+			destination, err := os.Create(filepath.Join(dir, dep.SHA256))
+			if err != nil {
+				sem <- fmt.Errorf("failed to create destination file: %s", err)
+				// return nil, fmt.Errorf("failed to create destination file: %s", err)
+				return
+			}
 
-		err = destination.Close()
-		if err != nil {
-			return nil, fmt.Errorf("failed to close dependency destination: %s", err)
-		}
+			_, err = io.Copy(destination, validatedSource)
+			if err != nil {
+				sem <- fmt.Errorf("failed to copy dependency: %s", err)
+				return
+				// return nil, fmt.Errorf("failed to copy dependency: %s", err)
+			}
 
-		err = source.Close()
-		if err != nil {
-			return nil, fmt.Errorf("failed to close dependency source: %s", err)
-		}
+			err = destination.Close()
+			if err != nil {
+				sem <- fmt.Errorf("failed to close dependency destination: %s", err)
+				return
+				// return nil, fmt.Errorf("failed to close dependency destination: %s", err)
+			}
 
-		dep.URI = fmt.Sprintf("file:///dependencies/%s", dep.SHA256)
-		dependencies = append(dependencies, dep)
+			err = source.Close()
+			if err != nil {
+				sem <- fmt.Errorf("failed to close dependency source: %s", err)
+				return
+				// return nil, fmt.Errorf("failed to close dependency source: %s", err)
+			}
+
+			dep.URI = fmt.Sprintf("file:///dependencies/%s", dep.SHA256)
+			dependencies[i] = dep
+			sem <- nil
+			// dependencies = append(dependencies, dep)
+		}(i, dep)
 	}
+
+	// wait for goroutines to finish
+	for i := 0; i < len(deps); i++ {
+		err := <-sem
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	dc.logger.Break()
+
+	dc.logger.Subprocess("Downloading complete")
 
 	dc.logger.Break()
 
