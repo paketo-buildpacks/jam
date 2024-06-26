7e59715ef61b7afc0381313647c4a38afdb6949f
Costas Papastathis <papastathiscr@gmail.com>
2024-06-26 15:14:05 +0300

    adding extension formatter

---
+++ internal/extension_formatter.go
@@ -0,0 +1,115 @@
+package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"sort"
+	"strings"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/paketo-buildpacks/packit/v2/cargo"
+)
+
+type ExtensionFormatter struct {
+	writer io.Writer
+}
+
+func NewExtensionFormatter(writer io.Writer) ExtensionFormatter {
+	return ExtensionFormatter{
+		writer: writer,
+	}
+}
+
+func printExtensionImplementation(writer io.Writer, config cargo.ExtensionConfig) {
+
+	if len(config.Metadata.DefaultVersions) > 0 {
+		fmt.Fprintf(writer, "#### Default Dependency Versions:\n| ID | Version |\n|---|---|\n")
+		var sortedDependencies []string
+		for key := range config.Metadata.DefaultVersions {
+			sortedDependencies = append(sortedDependencies, key)
+		}
+
+		sort.Strings(sortedDependencies)
+
+		for _, key := range sortedDependencies {
+			fmt.Fprintf(writer, "| %s | %s |\n", key, config.Metadata.DefaultVersions[key])
+		}
+		fmt.Fprintln(writer)
+	}
+
+	if len(config.Metadata.Dependencies) > 0 {
+		infoMap := map[depKey][]string{}
+		for _, d := range config.Metadata.Dependencies {
+
+			key := depKey{d.ID, d.Version, d.Source}
+			_, ok := infoMap[key]
+			if !ok {
+				sort.Strings(d.Stacks)
+				infoMap[key] = d.Stacks
+			} else {
+				val := infoMap[key]
+				val = append(val, d.Stacks...)
+				sort.Strings(val)
+				infoMap[key] = val
+			}
+		}
+
+		var sorted []cargo.ConfigExtensionMetadataDependency
+		for key, stacks := range infoMap {
+			sorted = append(sorted, cargo.ConfigExtensionMetadataDependency{
+				ID:      key[0],
+				Version: key[1],
+				Stacks:  stacks,
+				Source:  key[2],
+			})
+		}
+
+		sort.Slice(sorted, func(i, j int) bool {
+			iVal := sorted[i]
+			jVal := sorted[j]
+
+			if iVal.ID == jVal.ID {
+				iVersion := semver.MustParse(iVal.Version)
+				jVersion := semver.MustParse(jVal.Version)
+
+				if iVersion.Equal(jVersion) {
+					iStacks := strings.Join(iVal.Stacks, " ")
+					jStacks := strings.Join(jVal.Stacks, " ")
+
+					return iStacks < jStacks
+				}
+
+				return iVersion.GreaterThan(jVersion)
+			}
+
+			return iVal.ID < jVal.ID
+		})
+
+		fmt.Fprintf(writer, "#### Dependencies:\n| Name | Version | Stacks | Source |\n|---|---|---|---|\n")
+		for _, d := range sorted {
+			fmt.Fprintf(writer, "| %s | %s | %s | %s |\n", d.ID, d.Version, strings.Join(d.Stacks, " "), d.Source)
+		}
+		fmt.Fprintln(writer)
+	}
+
+}
+
+func (f ExtensionFormatter) Markdown(entries []ExtensionMetadata) {
+
+	fmt.Fprintf(f.writer, "## %s %s\n", entries[0].Config.Extension.Name, entries[0].Config.Extension.Version)
+	fmt.Fprintf(f.writer, "\n**ID:** `%s`\n\n", entries[0].Config.Extension.ID)
+	fmt.Fprintf(f.writer, "**Digest:** `%s`\n\n", entries[0].SHA256)
+	printExtensionImplementation(f.writer, entries[0].Config)
+
+}
+
+func (f ExtensionFormatter) JSON(entries []ExtensionMetadata) {
+	var output struct {
+		Buildpackage cargo.ExtensionConfig `json:"buildpackage"`
+	}
+
+	output.Buildpackage = entries[0].Config
+
+	_ = json.NewEncoder(f.writer).Encode(&output)
+}
